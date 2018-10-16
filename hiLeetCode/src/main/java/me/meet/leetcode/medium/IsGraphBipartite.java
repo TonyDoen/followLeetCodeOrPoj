d15d88029c4976f887a83ac84a40cad76c55d80e
zhangxudongtony <zhangxudongtony@didiglobal.com>
2018-10-16 15:45:58 +0800

    add Letter Case Permutation

--- hiLeetCode/src/main/java/me/meet/leetcode/medium/IsGraphBipartite.java
+++ hiLeetCode/src/main/java/me/meet/leetcode/medium/IsGraphBipartite.java
@@ -3,6 +3,7 @@ package me.meet.leetcode.medium;
 import java.util.LinkedList;
 
 public final class IsGraphBipartite {
+    private IsGraphBipartite() {}
     /**
      * url:http://www.cnblogs.com/grandyang/p/8519566.html
      * url:https://leetcode.com/problems/is-graph-bipartite/discuss/115487/Java-Clean-DFS-solution-with-Explanation
@@ -44,7 +45,7 @@ public final class IsGraphBipartite {
      */
 
     /**
-     * 题意
+     * 题意: 是二分图么
      * 输入数组中的graph[i]，表示顶点i所有相邻的顶点，比如对于例子1来说，顶点0和顶点1，3相连，顶点1和顶点0，2相连，顶点2和结点1，3相连，顶点3和顶点0，2相连。这道题让我们验证给定的图是否是二分图，所谓二分图，就是可以将图中的所有顶点分成两个不相交的集合，使得同一个集合的顶点不相连。为了验证是否有这样的两个不相交的集合存在，我们采用一种很机智的染色法，大体上的思路是要将相连的两个顶点染成不同的颜色，一旦在染的过程中发现有两连的两个顶点已经被染成相同的颜色，说明不是二分图。这里我们使用两种颜色，分别用1和-1来表示，初始时每个顶点用0表示未染色，然后遍历每一个顶点，如果该顶点未被访问过，则调用递归函数，如果返回false，那么说明不是二分图，则直接返回false。如果循环退出后没有返回false，则返回true。在递归函数中，如果当前顶点已经染色，如果该顶点的颜色和将要染的颜色相同，则返回true，否则返回false。如果没被染色，则将当前顶点染色，然后再遍历与该顶点相连的所有的顶点，调用递归函数，如果返回false了，则当前递归函数的返回false，循环结束返回true，参见代码如下：
      */
     static boolean isBipartite1(int[][] graph) {
