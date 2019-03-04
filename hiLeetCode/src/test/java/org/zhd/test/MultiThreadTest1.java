6228899697e8c5390b72edf3d2afdd976d716c6d
zhangxudongtony <zhangxudongtony@didiglobal.com>
2019-03-04 17:32:07 +0800

    add threadlocal

--- hiLeetCode/src/test/java/org/zhd/test/MultiThreadTest1.java
+++ hiLeetCode/src/test/java/org/zhd/test/MultiThreadTest1.java
@@ -116,15 +116,135 @@ public final class MultiThreadTest1 {
             Thread.sleep(20);//modify 2000 to 20
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Thread.currentThread().interrupt();//这样处理比较好
         }
         thread.interrupt();//请求中断MyThread线程
     }
 
+    private static void testInterrupt2() {
+        class MyRunnable implements Runnable {
+            @Override
+            public void run() {
+                System.err.println("start work");
+                while (!Thread.currentThread().isInterrupted()) {
+                    System.out.println("doing work");
+                }
+                System.err.println("done work");
+            }
+        }
+
+        Thread thread = new Thread(new MyRunnable());
+        thread.start();
+        long i = System.currentTimeMillis();
+        while (System.currentTimeMillis() - i < 10 * 1000) {
+            thread.isAlive();
+        }
+        thread.interrupt();
+    }
+
+    /**
+     * 我们来看下线程中断最重要的 3 个方法，它们都是来自 Thread 类！
+     * <p>
+     * 1、java.lang.Thread#interrupt       中断目标线程，给目标线程发一个中断信号，线程被打上中断标记。
+     * 2、java.lang.Thread#isInterrupted() 判断目标线程是否被中断，不会清除中断标记。
+     * 3、java.lang.Thread#interrupted     判断目标线程是否被中断，会清除中断标记。
+     */
+    // 示例1（中断失败）
+    private static void testInterruptFail1() {
+        Thread thread = new Thread(() -> { // 没有对应的中断处理
+            while (true) { // 因为虽然给线程发出了中断信号，但程序中并没有响应中断信号的逻辑，所以程序不会有任何反应
+                Thread.yield();
+            }
+        });
+        thread.start();
+        thread.interrupt();
+    }
+
+    // 示例2：（中断成功）
+    private static void testInterruptSuccess1() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                Thread.yield();
+
+                // 响应中断
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Java技术栈线程被中断，程序退出。");
+                    return;
+                }
+            }
+        });
+        thread.start();
+        thread.interrupt();
+    }
+
+    // 示例3（中断失败）
+    private static void testInterruptFail2() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                // 响应中断
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Java技术栈线程被中断，程序退出。");
+                    return;
+                }
+
+                try {
+                    Thread.sleep(3000); // sleep() 方法被中断后会清除中断标记
+                } catch (InterruptedException e) {
+                    System.out.println("Java技术栈线程休眠被中断，程序退出。");
+                }
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
+
+    // 示例4（中断成功）
+    private static void testInterruptSuccess2() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                // 响应中断
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Java技术栈线程被中断，程序退出。");
+                    return;
+                }
+
+                try {
+                    Thread.sleep(3000); // sleep() 方法被中断后会清除中断标记
+                } catch (InterruptedException e) {
+                    System.out.println("Java技术栈线程休眠被中断，程序退出。");
+                    Thread.currentThread().interrupt(); // 中断目标线程，给目标线程发一个中断信号，线程被打上中断标记。
+                }
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+        thread.interrupt();
+    }
+
+
     public static void main(String[] args) {
 //        testLockSupportOuch();
 //        testInterrupt();
-        testInterrupt1();
+//        testInterrupt1();
+//        testInterrupt2();
+//        testInterruptFail1();
+//        testInterruptFail2();
+        testInterruptSuccess1();
+        testInterruptSuccess2();
+
         System.out.println("end.");
-//        ThreadLocal
+        ThreadLocal<String> local = new ThreadLocal<>();
+        local.set("tony");
+        System.out.println(local.get());
     }
 }
