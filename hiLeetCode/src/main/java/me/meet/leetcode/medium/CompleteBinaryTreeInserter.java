98986c55b7879d9c136b369b7bffdca2123b96e3
zhangxudong8 <zhangxudong8@guazi.com>
2019-12-25 16:27:05 +0800

    add CBTInserter3

--- hiLeetCode/src/main/java/me/meet/leetcode/medium/CompleteBinaryTreeInserter.java
+++ hiLeetCode/src/main/java/me/meet/leetcode/medium/CompleteBinaryTreeInserter.java
@@ -1,10 +1,18 @@
 package me.meet.leetcode.medium;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class CompleteBinaryTreeInserter {
+    static class TreeNode {
+        Integer val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(Integer val) {
+            this.val = val;
+        }
+    }
+
     /**
      * Complete Binary Tree Inserter 完全二叉树插入器
      * A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
@@ -12,29 +20,31 @@ public class CompleteBinaryTreeInserter {
      * CBTInserter(TreeNode root) initializes the data structure on a given tree with head node root;
      * CBTInserter.insert(int v) will insert a TreeNode into the tree with value node.val = v so that the tree remains complete, and returns the value of the parent of the inserted TreeNode;
      * CBTInserter.get_root() will return the head node of the tree.
-     *
+     * <p>
      * Example 1:
      * Input: inputs = ["CBTInserter","insert","get_root"], inputs = [[[1]],[2],[]]
      * Output: [null,1,[1,2]]
-     *
+     * <p>
      * Example 2:
      * Input: inputs = ["CBTInserter","insert","insert","get_root"], inputs = [[[1,2,3,4,5,6]],[7],[8],[]]
      * Output: [null,3,4,[1,2,3,4,5,6,7,8]]
-     *
+     * <p>
      * Note:
      * The initial given tree is complete and contains between 1 and 1000 nodes.
      * CBTInserter.insert is called at most 10000 times per test case.
      * Every value of a given or inserted node is between 0 and 5000.
-     *
-     *
+     * <p>
+     * <p>
      * 题意：完全二叉树插入器
      */
-    static class CBTInserter {
-        TreeNode root;
-        Deque<TreeNode> deque;
-        public CBTInserter(TreeNode root) {
+    static class CBTInserter1 {
+        final TreeNode root;
+        final Deque<TreeNode> deque;
+
+        public CBTInserter1(TreeNode root) {
             this.root = root;
-            deque = new LinkedList<>();
+            this.deque = new LinkedList<>();
+
             Queue<TreeNode> queue = new LinkedList<>();
             queue.offer(root);
 
@@ -68,23 +78,14 @@ public class CompleteBinaryTreeInserter {
         }
     }
 
-    static class TreeNode {
-        Integer val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(Integer val) {
-            this.val = val;
-        }
-    }
-
-    public static void main(String[] args) {
-        CBTInserter cbti1 = new CBTInserter(new TreeNode(1));
+    private static void testCBTInserter1() {
+        CBTInserter1 cbti1 = new CBTInserter1(new TreeNode(1));
         cbti1.insert(2);
         TreeNode res1 = cbti1.getRoot();
         System.out.println(res1);
 
-        int[] arr = new int[]{2,3,4,5,6};
-        CBTInserter cbti2 = new CBTInserter(new TreeNode(1));
+        int[] arr = new int[]{2, 3, 4, 5, 6};
+        CBTInserter1 cbti2 = new CBTInserter1(new TreeNode(1));
         for (int i : arr) {
             cbti2.insert(i);
         }
@@ -92,6 +93,112 @@ public class CompleteBinaryTreeInserter {
         cbti2.insert(8);
         TreeNode res2 = cbti2.getRoot();
         System.out.println(res2);
+    }
 
+
+//    static class CBTInserter2 {
+//        private final TreeNode root;
+//        private final Deque<TreeNode> q;
+//
+//        CBTInserter2(TreeNode root) {
+//            this.root = root;
+//            q = new LinkedList<>();
+//            q.push(root);
+//
+//            for (; !q.isEmpty(); ) {
+//                TreeNode t = q.peek();
+//                if (null == t.left || null == t.right) {
+//                    break;
+//                }
+//                q.push(t.left);
+//                q.push(t.right);
+//                q.pop();
+//            }
+//        }
+//
+//        int insert(int v) {
+//            TreeNode node = new TreeNode(v);
+//            TreeNode t = q.peek();
+//            if (null == t.left) {
+//                t.left = node;
+//            } else {
+//                t.right = node;
+//                q.push(t.left);
+//                q.push(t.right);
+//                q.pop();
+//            }
+//            return t.val;
+//        }
+//
+//        TreeNode getRoot() {
+//            return this.root;
+//        }
+//    }
+//
+//    private static void testCBTInserter2() {
+//        int[] arr = new int[]{2, 3, 4, 5, 6};
+//        CBTInserter2 cbti2 = new CBTInserter2(new TreeNode(1));
+//        for (int i : arr) {
+//            cbti2.insert(i);
+//        }
+//        cbti2.insert(7);
+//        cbti2.insert(8);
+//        TreeNode res2 = cbti2.getRoot();
+//        System.out.println(res2);
+//    }
+
+    static class CBTInserter3 {
+        final List<TreeNode> tree;
+        CBTInserter3(TreeNode root) {
+            this.tree = new LinkedList<>();
+
+            tree.add(root);
+            for (int i = 0; i < tree.size(); i++) {
+                TreeNode it = tree.get(i);
+                if (null != it.left) {
+                    tree.add(it.left);
+                }
+                if (null != it.right) {
+                    tree.add(it.right);
+                }
+            }
+        }
+
+        int insert(int v) {
+            TreeNode vt = new TreeNode(v);
+            int n = tree.size();
+
+            tree.add(vt);
+            TreeNode tmp = tree.get((n-1)/2);
+            if (n % 2 == 1) {
+                tmp.left = vt;
+            } else {
+                tmp.right = vt;
+            }
+            return tmp.val;
+        }
+
+        TreeNode getRoot() {
+            return tree.get(0);
+        }
+    }
+
+    private static void testCBTInserter3() {
+        CBTInserter3 cbti2 = new CBTInserter3(new TreeNode(1));
+
+        int[] arr = new int[]{2, 3, 4, 5, 6};
+        for (int i : arr) {
+            cbti2.insert(i);
+        }
+        cbti2.insert(7);
+        cbti2.insert(8);
+        TreeNode res2 = cbti2.getRoot();
+        System.out.println(res2);
+    }
+
+    public static void main(String[] args) {
+        testCBTInserter1();
+//        testCBTInserter2();
+        testCBTInserter3();
     }
 }
